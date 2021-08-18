1491e8ed6effb8c929f5be483e4e597b67ac1c66
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-18 13:22:10 -0500

    completed assignment

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Joey, can you do this?");
+
+            var response = Console.ReadLine();
+
+            Console.WriteLine(response);
+        }
+    }
+}
